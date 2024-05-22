7c7b5dc4cf3de768eea38cf6a779194d23312d61
why <435371447@qq.com>
2024-05-22 20:38:18 +0800

    system call

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -10,97 +10,53 @@
 //! `sys_` then the name of the syscall. You can find functions like this in
 //! submodules, and you should also implement syscalls this way.
 
-/// openat syscall
+/// fs
+pub const SYSCALL_GETCWD: usize = 17;
+pub const SYSCALL_PIPE: usize = 59;
+pub const SYSCALL_DUP: usize = 23;
+pub const SYSCALL_DUP3: usize = 24;
+pub const SYSCALL_CHDIR: usize = 49;
 pub const SYSCALL_OPENAT: usize = 56;
-/// close syscall
 pub const SYSCALL_CLOSE: usize = 57;
-/// read syscall
+pub const SYSCALL_GENDENTS64: usize = 61;
 pub const SYSCALL_READ: usize = 63;
-/// write syscall
 pub const SYSCALL_WRITE: usize = 64;
-/// unlinkat syscall
-pub const SYSCALL_UNLINKAT: usize = 35;
-/// linkat syscall
 pub const SYSCALL_LINKAT: usize = 37;
-/// fstat syscall
+pub const SYSCALL_UNLINKAT: usize = 35;
+pub const SYSCALL_MKDIRAT: usize = 34;
+pub const SYSCALL_UMOUNT2: usize = 39;
+pub const SYSCALL_MOUNT: usize = 40;
 pub const SYSCALL_FSTAT: usize = 80;
-/// exit syscall
+/// process
+pub const SYSCALL_FORK: usize = 220;
+pub const SYSCALL_CLONE: usize = 220;
+pub const SYSCALL_EXEC: usize = 221;
+pub const SYSCALL_WAIT4: usize = 260;
 pub const SYSCALL_EXIT: usize = 93;
-/// sleep syscall
-pub const SYSCALL_SLEEP: usize = 101;
-/// yield syscall
+pub const SYSCALL_GETPPID: usize = 173;
+pub const SYSCALL_GETPID: usize = 172;
+/// mm
+pub const SYSCALL_BRK: usize = 214;
+pub const SYSCALL_MUNMAP: usize = 215;
+pub const SYSCALL_MMAP: usize = 222;
+/// others
+pub const SYSCALL_TIMES: usize = 153;
+pub const SYSCALL_UNAME: usize = 160;
 pub const SYSCALL_YIELD: usize = 124;
+pub const SYSCALL_GETTIMEOFDAY: usize = 169;
+pub const SYSCALL_SLEEP: usize = 101;
+
 /// kill syscall
 pub const SYSCALL_KILL: usize = 129;
-/*
-/// sigaction syscall
-pub const SYSCALL_SIGACTION: usize = 134;
-/// sigprocmask syscall
-pub const SYSCALL_SIGPROCMASK: usize = 135;
-/// sigreturn syscall
-pub const SYSCALL_SIGRETURN: usize = 139;
-*/
-/// gettimeofday syscall
-pub const SYSCALL_GETTIMEOFDAY: usize = 169;
-/// getpid syscall
-pub const SYSCALL_GETPID: usize = 172;
 /// gettid syscall
 pub const SYSCALL_GETTID: usize = 178;
-pub const SYSCALL_CLONE: usize = 220;
-/// fork syscall
-pub const SYSCALL_FORK: usize = 220;
-/// exec syscall
-pub const SYSCALL_EXEC: usize = 221;
-/// waitpid syscall
-pub const SYSCALL_WAITPID: usize = 260;
-/// set priority syscall
-pub const SYSCALL_SET_PRIORITY: usize = 140;
-/*
-/// sbrk syscall
-pub const SYSCALL_SBRK: usize = 214;
-*/
-/// munmap syscall
-pub const SYSCALL_MUNMAP: usize = 215;
-/// mmap syscall
-pub const SYSCALL_MMAP: usize = 222;
 /// spawn syscall
 pub const SYSCALL_SPAWN: usize = 400;
-/*
-/// mail read syscall
-pub const SYSCALL_MAIL_READ: usize = 401;
-/// mail write syscall
-pub const SYSCALL_MAIL_WRITE: usize = 402;
-*/
-/// dup syscall
-pub const SYSCALL_DUP: usize = 24;
-/// pipe syscall
-pub const SYSCALL_PIPE: usize = 59;
-/// task info syscall
-pub const SYSCALL_TASK_INFO: usize = 410;
 /// thread_create syscall
 pub const SYSCALL_THREAD_CREATE: usize = 460;
 /// waittid syscall
 pub const SYSCALL_WAITTID: usize = 462;
-/// mutex_create syscall
-pub const SYSCALL_MUTEX_CREATE: usize = 463;
-/// mutex_lock syscall
-pub const SYSCALL_MUTEX_LOCK: usize = 464;
-/// mutex_unlock syscall
-pub const SYSCALL_MUTEX_UNLOCK: usize = 466;
-/// semaphore_create syscall
-pub const SYSCALL_SEMAPHORE_CREATE: usize = 467;
-/// semaphore_up syscall
-pub const SYSCALL_SEMAPHORE_UP: usize = 468;
-/// enable deadlock detect syscall
-pub const SYSCALL_ENABLE_DEADLOCK_DETECT: usize = 469;
-/// semaphore_down syscall
-pub const SYSCALL_SEMAPHORE_DOWN: usize = 470;
-/// condvar_create syscall
-pub const SYSCALL_CONDVAR_CREATE: usize = 471;
-/// condvar_signal syscall
-pub const SYSCALL_CONDVAR_SIGNAL: usize = 472;
-/// condvar_wait syscallca
-pub const SYSCALL_CONDVAR_WAIT: usize = 473;
+
 
 // Not standard POSIX sys_call
 const SYSCALL_SHUTDOWN: usize = 2000;
@@ -122,7 +78,11 @@ use crate::fs::Stat;
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
     match syscall_id {
+        SYSCALL_GETCWD => -1,
         // SYSCALL_DUP => sys_dup(args[0]),
+        SYSCALL_DUP => -1,
+        SYSCALL_DUP3 => -1,
+        SYSCALL_CHDIR => -1,
         SYSCALL_LINKAT => sys_linkat(args[1] as *const u8, args[3] as *const u8),
         SYSCALL_UNLINKAT => sys_unlinkat(args[1] as *const u8),
         SYSCALL_OPEN => sys_openat(AT_FDCWD, args[0] as *const u8, args[1] as u32, 0o777u32),
@@ -133,7 +93,12 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
             args[3] as u32,
         ),
         SYSCALL_CLOSE => sys_close(args[0]),
+        SYSCALL_GENDENTS64 => -1,
         // SYSCALL_PIPE => sys_pipe(args[0] as *mut usize),
+        SYSCALL_PIPE => -1,
+        SYSCALL_MKDIRAT => -1,
+        SYSCALL_UMOUNT2 => -1,
+        SYSCALL_MOUNT => -1,
         SYSCALL_READ => sys_read(args[0], args[1] as *const u8, args[2]),
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_FSTAT => sys_fstat(args[0], args[1] as *mut Stat),
@@ -142,34 +107,19 @@ pub fn syscall(syscall_id: usize, args: [usize; 6]) -> isize {
         SYSCALL_YIELD => sys_yield(),
         SYSCALL_GETPID => sys_getpid(),
         SYSCALL_GETTID => sys_gettid(),
-        SYSCALL_CLONE => sys_clone(
-            args[0] as u32,
-            args[1] as *const u8,
-            args[2] as *const u32,
-            args[3] as *const usize,
-            args[4] as *const u32,
-        ),
         SYSCALL_FORK => sys_fork(),
         SYSCALL_EXEC => sys_exec(args[0] as *const u8, args[1] as *const usize, args[2] as *const usize,),
-        SYSCALL_WAITPID => sys_waitpid(args[0] as isize, args[1] as *mut i32),
+        SYSCALL_WAIT4 => sys_waitpid(args[0] as isize, args[1] as *mut i32),
+        SYSCALL_GETPPID => -1,
         SYSCALL_GETTIMEOFDAY => sys_get_time(args[0] as *mut TimeVal, args[1]),
+        SYSCALL_BRK => -1,
         SYSCALL_MMAP => sys_mmap(args[0], args[1], args[2]),
         SYSCALL_MUNMAP => sys_munmap(args[0], args[1]),
-        SYSCALL_SET_PRIORITY => sys_set_priority(args[0] as isize),
-        SYSCALL_TASK_INFO => sys_task_info(args[0] as *mut TaskInfo),
+        SYSCALL_TIMES => -1,
+        SYSCALL_UNAME => -1,
         SYSCALL_SPAWN => sys_spawn(args[0] as *const u8),
         SYSCALL_THREAD_CREATE => sys_thread_create(args[0], args[1]),
         SYSCALL_WAITTID => sys_waittid(args[0]) as isize,
-        SYSCALL_MUTEX_CREATE => sys_mutex_create(args[0] == 1),
-        SYSCALL_MUTEX_LOCK => sys_mutex_lock(args[0]),
-        SYSCALL_MUTEX_UNLOCK => sys_mutex_unlock(args[0]),
-        SYSCALL_SEMAPHORE_CREATE => sys_semaphore_create(args[0]),
-        SYSCALL_SEMAPHORE_UP => sys_semaphore_up(args[0]),
-        SYSCALL_ENABLE_DEADLOCK_DETECT => sys_enable_deadlock_detect(args[0]),
-        SYSCALL_SEMAPHORE_DOWN => sys_semaphore_down(args[0]),
-        SYSCALL_CONDVAR_CREATE => sys_condvar_create(),
-        SYSCALL_CONDVAR_SIGNAL => sys_condvar_signal(args[0]),
-        SYSCALL_CONDVAR_WAIT => sys_condvar_wait(args[0], args[1]),
         SYSCALL_KILL => sys_kill(args[0], args[1] as u32),
         SYSCALL_SHUTDOWN => sys_shutdown(args[0] != 0),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
